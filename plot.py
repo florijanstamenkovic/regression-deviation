bf6581430f283476a6db92ec80462e1e27aa3f3a
Florijan Stamenković <florijan.stamenkovic@gmail.com>
2018-10-30 12:31:06 +0100

    Improve plotting

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
-def plot_error_at_retrieval(log_prob, stddev, model_name, points=41):
+def plot_error_at_retrieval(mae, stddev, model_name, points=41):
     confidence = stddev.max() - stddev
     confidence -= confidence.min()
     confidence /= confidence.max()
@@ -28,14 +28,19 @@ def plot_error_at_retrieval(log_prob, stddev, model_name, points=41):
     for point in space:
         conf_thresh = np.interp(point, retrieval, space)
         mask = confidence >= conf_thresh
-        errors.append(0 if mask.sum() == 0 else log_prob[mask].mean())
+        errors.append(0 if mask.sum() == 0 else mae[mask].mean())
+
+    # Don't plot zero retrievals
+    mask = space >= 0.1
+    space = space[mask]
+    errors = np.array(errors)[mask]
 
     plt.figure(figsize=(12, 9))
-    plt.title("Log-prob at retrieval: " + model_name)
+    plt.title("MAE at retrieval: " + model_name)
     plt.plot(space, errors)
-    plt.ylabel("Log-prob")
+    plt.ylabel("MAE")
     plt.xlabel("Retrieval")
     plt.grid(alpha=0.5, linestyle=':')
-    plt.savefig(os.path.join(OUTPUT_DIR, "log_prob_at_retrieval_%s.pdf" %
+    plt.savefig(os.path.join(OUTPUT_DIR, "mae_at_retrieval_%s.pdf" %
                              model_name), bbox_inches='tight')
     plt.close()
