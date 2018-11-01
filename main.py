d06e32f4edb913acdaece4fa679c1e3a9697fba3
Florijan Stamenković <florijan.stamenkovic@gmail.com>
2018-11-01 18:24:34 +0100

    Add target scaling

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ import plot
 
 
 MODELS = (
-    (models.ConfidenceRegressor, "dummy",
+    (models.ConfidenceRegressor, "dummy", False,
      {
          "regression_cls": [sklearn.dummy.DummyRegressor],
          "reg_conf_split": [0.5],
          "stddev_cls": [sklearn.dummy.DummyRegressor],
      }),
-    (models.ConfidenceRegressor, "ridge",
+    (models.ConfidenceRegressor, "ridge", False,
      {
          "regression_cls": [sklearn.linear_model.Ridge],
          "regression__alpha": [0, 1, 10, 100],
@@ -34,13 +34,13 @@ MODELS = (
          "stddev_cls": [sklearn.linear_model.Ridge],
          "stddev__alpha": [0, 1, 10, 100],
      }),
-    (models.ConfidenceRegressor, "linear",
+    (models.ConfidenceRegressor, "linear", False,
      {
          "regression_cls": [sklearn.linear_model.LinearRegression],
          "reg_conf_split": [0.5],
          "stddev_cls": [sklearn.linear_model.LinearRegression],
      }),
-    (models.ConfidenceRegressor, "rf",
+    (models.ConfidenceRegressor, "rf", False,
      {
          "regression_cls": [sklearn.ensemble.RandomForestRegressor],
          "regression__n_estimators": [100],
@@ -48,13 +48,13 @@ MODELS = (
          "stddev_cls": [sklearn.ensemble.RandomForestRegressor],
          "stddev__n_estimators": [100],
      }),
-    (models.GaussianProcessEnsemble, "gaussian_process",
+    (models.GaussianProcessEnsemble, "gaussian_process", False,
      {
      }),
-    (models.BayesianRidge, "bayesian_ridge",
+    (models.BayesianRidge, "bayesian_ridge", False,
      {
      }),
-    (models.TorchRegressor, "torch",
+    (models.TorchRegressor, "torch", True,
      {
      }),
 )
@@ -75,8 +75,6 @@ def parse_args():
                       help="If the dataset should be reduced for debugging.")
     argp.add_argument("--test-size", type=float, default=0.4,
                       help="Part of the dataset that's the test set")
-    argp.add_argument("--scale-target", action="store_true",
-                      help="If the target should be zero-mean unit-dev")
     return argp.parse_args()
 
 
@@ -100,16 +98,19 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=args.test_size, random_state=2345)
 
-    # y = y - y.mean()
-    # y = y / y.std()
-
     if args.model is None:
         used_models = models.MODELS
     else:
         used_models = [m for m in MODELS if m[1] in args.model]
 
-    for model_cls, model_name, params in used_models:
+    for model_cls, model_name, scale_target, params in used_models:
         logging.info("Fitting model %s", model_name)
+
+        if scale_target:
+            y_mean = y_train.mean()
+            y_std = y_train.std()
+            y_train = (y_train - y_mean) / y_std
+            y_test = (y_test - y_mean) / y_std
 
         grid_search = GridSearchCV(
             model_cls(), params, cv=5, n_jobs=args.n_jobs,
@@ -125,7 +126,11 @@ def main():
             X_test, prediction)
 
         log_probs = models.normpdf(y_test, stddev, prediction, True)
-        mae = np.abs(prediction - y_test)
+
+        if scale_target:
+            mae = np.abs(prediction - (y_test * y_std + y_mean))
+        else:
+            mae = np.abs(prediction - y_test)
 
         logging.info("Model: %s, MAE: %.2f, mean log-prob: %.2f",
                      model_name, mae.mean(), log_probs.mean())
