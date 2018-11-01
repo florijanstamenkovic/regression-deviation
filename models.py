51d0ddd101381ac3ec75399af92b0bf3f9f4b727
Florijan Stamenković <florijan.stamenkovic@gmail.com>
2018-11-01 19:33:02 +0100

    Update predictors

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+
 def concat(ftrs, prediction):
     return np.hstack((ftrs, prediction.reshape(-1, 1)))
 
@@ -85,7 +86,7 @@ class BayesianRidge():
         self.ridge = sklearn.linear_model.BayesianRidge(**params)
         self.params = params
 
-    def fit(self, X,  y):
+    def fit(self, X, y):
         self.ridge.fit(X, y)
 
     def predict(self, X):
@@ -152,13 +153,18 @@ class TorchRegressor(nn.Module):
         super(TorchRegressor, self).__init__()
         self.set_params(**params)
         self.mnb_size = 512
-        self.epochs = 20
+        self.epochs = 50
 
-        self.mean_hid = nn.Linear(17, 512)
-        self.mean_do = nn.Dropout(0.2)
-        self.mean = nn.Linear(17, 1)
-        self.stddev = nn.Linear(17, 1)
+    def init_params(self, X, y):
+        self.mean = nn.Linear(X.shape[1], 1)
+        self.stddev = nn.Linear(X.shape[1], 1)
         self.optimizer = optim.RMSprop(self.parameters(), lr=0.001)
+
+        self.mean.bias.data.fill_(y.mean())
+        self.stddev.bias.data.fill_(y.std())
+        init = 0.2
+        self.mean.weight.data.uniform_(-init, init)
+        self.stddev.weight.data.uniform_(-init, init)
 
     def prob(self, x, mean, stddev):
         var = stddev ** 2
@@ -166,14 +172,14 @@ class TorchRegressor(nn.Module):
         num = (-((x - mean) ** 2 / (2 * var))).exp()
         return num / denom + 0.000001
 
-    def fit(self, X, y):
-        logging.info("Target mean: %.2f, stddev: %.2f",
-                     y.mean(), y.std())
+    def forward_mean(self, x):
+        return self.mean(x)
 
-        self.mean.bias.data.fill_(y.mean())
-        self.mean.weight.data.uniform_(0)
-        self.stddev.bias.data.fill_(y.std())
-        self.stddev.weight.data.fill_(0)
+    def forward_std(self, x):
+        return self.stddev(x)
+
+    def fit(self, X, y):
+        self.init_params(X, y)
 
         def batches():
             inds = np.random.shuffle(np.arange(len(X)))
@@ -186,20 +192,24 @@ class TorchRegressor(nn.Module):
         for epoch_ind in range(self.epochs):
             epoch_losses = []
             for X_mnb, y_mnb in batches():
-                # mean = self.mean(self.mean_do(self.mean_hid(X_mnb)))
-                mean = self.mean(X_mnb)
-                stddev = self.stddev(X_mnb)
-                # logging.info("%.3f, %.3f", mean.mean().item(), stddev.mean().item())
+                mean = self.forward_mean(X_mnb)
+                stddev = self.forward_std(X_mnb)
                 prob = self.prob(y_mnb, mean, stddev)
+                logging.debug("%.3f", prob.mean().item())
                 loss = -prob.log().mean()
                 epoch_losses.append(loss.item())
 
                 self.optimizer.zero_grad()
                 loss.backward()
+                logging.debug("%.3f, %.3f, %.3f, %.3f",
+                              self.mean.weight.grad.mean().item(),
+                              self.mean.weight.mean().item(),
+                              self.stddev.weight.grad.mean().item(),
+                              self.stddev.weight.mean().item())
                 self.optimizer.step()
 
             logging.info("TorchRegressor epoch %d, loss %.5f",
-                         epoch_ind, np.mean(epoch_losses))
+                         epoch_ind, np.mean(epoch_losses[0]))
 
     def set_params(self, **params):
         self.params = params
@@ -210,8 +220,10 @@ class TorchRegressor(nn.Module):
 
     def predict(self, X):
         with torch.no_grad():
-            return self.mean(torch.FloatTensor(X)).cpu().numpy().flatten()
+            return self.forward_mean(
+                torch.FloatTensor(X)).cpu().numpy().flatten()
 
     def predict_stddev(self, X, _):
         with torch.no_grad():
-            return self.stddev(torch.FloatTensor(X)).cpu().numpy().flatten()
+            return self.forward_std(
+                torch.FloatTensor(X)).cpu().numpy().flatten()
