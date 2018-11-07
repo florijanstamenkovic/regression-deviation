0280c6569134a477153aae1a9b13cd0c2eb5d639
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2018-11-07 11:34:26 +0100

    Plot density functions and derivatives

--- plot.py
+++ plot.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import math
 import os
 
 import numpy as np
@@ -7,16 +10,21 @@ matplotlib.use('Agg')  # ensure headless operation
 from matplotlib import pyplot as plt
 
 
-OUTPUT_DIR = "output"
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
+FIGSIZE = (8, 6)
+
+
+def savefig(name):
+    out_dir = "output"
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+    plt.savefig(os.path.join(out_dir, "%s.pdf" % name), bbox_inches='tight')
 
 
 def plot_error_at_retrieval(abs_errors, stddevs, model_names, rmse, points=41):
 
     error_name = "RMSE" if rmse else "MAE"
 
-    plt.figure(figsize=(12, 9))
+    plt.figure(figsize=FIGSIZE)
     plt.title("%s at retrieval" % error_name)
 
     for abs_error, stddev, model_name in zip(abs_errors, stddevs, model_names):
@@ -47,18 +55,73 @@ def plot_error_at_retrieval(abs_errors, stddevs, model_names, rmse, points=41):
     plt.xlabel("Retrieval")
     plt.grid(alpha=0.5, linestyle=':')
     plt.legend()
-    plt.savefig(os.path.join(OUTPUT_DIR, "%s_at_retrieval.pdf" % error_name),
-                bbox_inches='tight')
+    savefig("%s_at_retrieval" % error_name)
     plt.close()
 
 
 def plot_stddev_error_scatter(abs_error, stddev, model_name):
-    plt.figure(figsize=(12, 9))
+    plt.figure(figsize=FIGSIZE)
     plt.title("Deviation vs error scatter")
     plt.scatter(abs_error, stddev, s=2.0, alpha=0.2)
     plt.xlabel("Absolute error")
     plt.ylabel("Estimated standard deviation")
     plt.grid(alpha=0.5, linestyle=':')
-    plt.savefig(os.path.join(OUTPUT_DIR, "std_scatter_%s.pdf" % model_name),
-                bbox_inches='tight')
+    savefig("std_scatter_%s" % model_name)
     plt.close()
+
+
+
+def plot_pdfs():
+    m = 0
+    s = 1
+
+    def gexp():
+        return np.exp(-((x - m) ** 2)) / (2 * s)
+
+    def gauss():
+        return gexp() / (2 * math.pi * s) ** 0.5
+
+    def gauss_dm():
+        return gexp() * (x - m) / (2 * math.pi * s ** 3) ** 0.5
+
+    def gauss_ds():
+        return -gexp() * (s - (x - m) ** 2) / (2 * math.pi * s ** 5) ** 0.5
+
+    def plot_gauss(name, plot_points):
+        plt.figure(figsize=FIGSIZE)
+        plt.title("Gaussian probability density function, mean=0, stddev=1")
+        plt.plot(x, gauss(), label="N(m, s)")
+        plt.plot(x, gauss_dm(), label="d/dm N(m, s)")
+        plt.plot(x, gauss_ds(), label="d/ds N(m, s)")
+        if plot_points:
+            def plot_point(x, linestyle, label):
+                plt.plot([x, x], [-0.2, 0.2], color="r", linestyle=linestyle,
+                         label=label)
+            plot_point(0.1, ":", "x0")
+            plot_point(1, "-.", "x1")
+            plot_point(2, "--", "x2")
+        plt.grid(alpha=0.5, linestyle=':')
+        plt.legend()
+        savefig(name)
+        plt.close()
+
+    x = np.linspace(-3, 3, 300)
+    plot_gauss("gauss", False)
+
+    x = np.linspace(0, 3, 300)
+    plot_gauss("gauss_points", True)
+
+    x = np.linspace(-2, 2, 300)
+    plt.figure(figsize=FIGSIZE)
+    plt.title("Log of Gaussian probability density function, mean=0, stddev=1")
+    plt.plot(x, np.log(gauss()), label="ln(N(m, s))")
+    plt.plot(x, (x - m) / s, label="d/dm ln(N(m, s))")
+    plt.plot(x, ((x - m) ** 2 - s) / (2 * s ** 2), label="d/ds ln(N(m, s))")
+    plt.grid(alpha=0.5, linestyle=':')
+    plt.legend()
+    savefig("ln_gauss")
+    plt.close()
+
+
+if __name__ == "__main__":
+    plot_pdfs()
